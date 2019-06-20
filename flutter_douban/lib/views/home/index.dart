dd41c9a467d30cc4eb43e1b819b18bab78bc4719
howardwzh <howardwzh@163.com>
2019-06-20 18:35:30 +0800

    feature: 增加分类组件逻辑

--- flutter_douban/lib/views/home/index.dart
+++ flutter_douban/lib/views/home/index.dart
@@ -15,12 +15,12 @@ class _HomeState extends State<Home> {
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-        MySwiper(),
-        FlashDeals(),
-        Hot(),
-        Sort(title: '服装'),
-        Sort(title: '家电'),
-        Sort(title: '百货'),
+          MySwiper(),
+          FlashDeals(),
+          Hot(),
+          Sort(title: '服装', subclass: [{'id': '1', 'name': '男装'}, {'id': '2', 'name': '女装'}, {'id': '2', 'name': '童装'}, {'id': '2', 'name': '内衣'}]),
+          Sort(title: '家电', subclass: [{'id': '3', 'name': '电视'}, {'id': '4', 'name': '空调'}, {'id': '4', 'name': '洗衣机'}, {'id': '4', 'name': '冰箱'}, {'id': '4', 'name': '厨卫大电'}]),
+          Sort(title: '百货', subclass: [{'id': '5', 'name': '新鲜水果'},{'id': '6', 'name': '蔬菜蛋品'},{'id': '6', 'name': '精选肉类'},{'id': '6', 'name': '海鲜水产'},{'id': '6', 'name': '冷饮冻食'},{'id': '6', 'name': '蔬菜蛋品'},{'id': '6', 'name': '中外名酒'},{'id': '6', 'name': '进口食品'}]),
         ]
       )
     );
--- flutter_douban/lib/views/home/sort.dart
+++ flutter_douban/lib/views/home/sort.dart
@@ -1,18 +1,74 @@
 import 'package:flutter/material.dart';
 
-class Sort extends StatelessWidget {
+class Sort extends StatefulWidget {
   final String title;
-  const Sort({Key key, this.title}) : super(key: key);
+  final List subclass;
+  const Sort({Key key, this.title, this.subclass}) : super(key: key);
+
+  @override
+  _SortState createState() => _SortState();
+}
+
+class _SortState extends State<Sort> with SingleTickerProviderStateMixin {
+  final List<Tab> myTabs = <Tab>[];
+  TabController _tabController;
+
+  @override
+  void initState() { 
+    super.initState();
+    for (int i = 0; i < widget.subclass.length; i++) {
+      myTabs.add(Tab(
+        text: widget.subclass[i]['name']
+      ));
+    }
+    _tabController = TabController(vsync: this, length: myTabs.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 200,
       padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-      child: Container(
-        child: Text('TODO: 分类展示-${this.title}'),
-        height: 200,
-        alignment: Alignment.topLeft,
-        color: Colors.redAccent,
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 50,
+            child: Row(children: <Widget>[
+              Container(
+                width: 60,
+                child: Text(widget.title, style: TextStyle(fontSize: 20.0))
+              ),
+              Expanded(
+                child: TabBar(
+                  controller: _tabController,
+                  tabs: myTabs,
+                  labelColor: Colors.blueAccent,
+                  unselectedLabelColor: Colors.black,
+                  isScrollable: true
+                )
+              )
+            ],)  
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: myTabs.map((Tab tab) {
+                return Row(children: <Widget>[
+                  Expanded(child: Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill, height: 150,),),
+                  Expanded(child: Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill, height: 150),),
+                  Expanded(child: Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill, height: 150),),
+                ]);
+              }).toList(),
+            )
+          )
+        ],
       )
     );
   }
