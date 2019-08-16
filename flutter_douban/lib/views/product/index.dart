c21b6f95df550d67eeedb05c68174f35a3fff003
howardwzh <howardwzh@163.com>
2019-08-16 18:09:48 +0800

    feature: 增加详情页底部操作栏

--- flutter_douban/lib/views/product/index.dart
+++ flutter_douban/lib/views/product/index.dart
@@ -23,7 +23,96 @@ class _ProductState extends State<Product> {
           MySwiper(dataList: [{'imgSrc': 'assets/images/banner-cera.jpg'},{'imgSrc': 'assets/images/banner-cera.jpg'},{'imgSrc': 'assets/images/banner-cera.jpg'},{'imgSrc': 'assets/images/banner-cera.jpg'}]),
           ImageList(),
         ],
-      )
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child:FlatButton(
+                padding: EdgeInsets.all(0.0),
+                child: Container(
+                  height: 60.0,
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Icon(Icons.phone),
+                        Text('联系客服')
+                      ],
+                    ),
+                  ),
+                ),
+                onPressed: (){
+                  print('xxxx');
+                }
+              )
+            ),
+            Expanded(
+              child:FlatButton(
+                padding: EdgeInsets.all(0.0),
+                child: Container(
+                  height: 60.0,
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Icon(Icons.home),
+                        Text('进店')
+                      ],
+                    ),
+                  ),
+                ),
+                onPressed: (){
+                  print('xxxx');
+                }
+              )
+            ),
+            Expanded(
+              child:FlatButton(
+                child: Container(
+                  height: 60.0,
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Icon(Icons.shopping_cart),
+                        Text('购物车')
+                      ],
+                    ),
+                  ),
+                ),
+                onPressed: (){
+                  print('xxxx');
+                }
+              )
+            ),
+            Container(
+              height: 60.0,
+              width:100.0,
+              decoration: BoxDecoration(color: Color.fromRGBO(255, 175, 2, 1.0)),
+              child: FlatButton(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Text('加入购物车', style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1.0), fontSize: 16.0),),
+                onPressed: (){
+                  print('zzzz');
+                },
+              ),
+            ),
+             Container(
+              height: 60.0,
+              width: 90.0,
+              decoration: BoxDecoration(color: Color.fromRGBO(255, 31, 1, 1.0)),
+              child: FlatButton(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Text('立即购买', style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1.0), fontSize: 16.0),),
+                onPressed: (){
+                  print('zzzz');
+                },
+              ),
+            )
+          ],
+        )
+      ),
     );
   }
 }
